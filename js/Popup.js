d011b394326e4d522e44279acc1ce760ab9b3d07
Damien Martinez <damienca.martinez@gmail.com>
2020-11-04 14:07:16 +0100

    Prototype Popup

---
+++ js/Popup.js
@@ -0,0 +1,10 @@
+function Popup()
+{
+    document.getElementsByClassName('Popup');
+}
+
+function ClosePopup()
+{
+    var Popup=document.getElementsByClassName('Popup');
+    Popup.style.display="none";
+}
